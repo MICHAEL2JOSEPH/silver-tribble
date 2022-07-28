2613760eb467c83fba668c54d2eb9c00875c2f8c
mike-joe-webdeveloper <monmike11@gmail.com>
2022-07-28 14:51:29 +0100

    MikeJoeFrame

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <HeaderPageslist />  
+    <HeaderBackground />
+    <Nuxt />
+
+    <!-- <HeaderCopyright /> -->
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
